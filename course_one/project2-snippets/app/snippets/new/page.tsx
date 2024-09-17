bfa18d052c71f26353e4fbc3597e5d6e96e12add
Brian Bravol <lumalabravo@gmail.com>
2024-09-17 12:40:05 +0300

    using sqldatabase to store data locally

--- course_one/project2-snippets/app/snippets/new/page.tsx
+++ course_one/project2-snippets/app/snippets/new/page.tsx
@@ -1,5 +1,54 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function SnippetCreatePage() {
-  return <div>Create a snippet!</div>;
+  async function createSnippet(formData: FormData) {
+    // this needs to be a server action!
+    "use server";
+    //create the user's inputs and make sure they are valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    //create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+    // redirect the user back to the root route
+    redirect("/");
+  }
+  return (
+    <form action={createSnippet}>
+      <h3 className=" font-bold m-3">Create a Snippet!</h3>
+      <div className="flex flex-col gap-4 m-3">
+        <div className="flex gap-4">
+          <label className=" w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className=" w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+        {/* button */}
+        <button type="submit" className="rounded p-3 bg-blue-500 text-white">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
