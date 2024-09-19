b121165318ea97b8077d38308db3ec2c90a9b95c
Brian Bravol <lumalabravo@gmail.com>
2024-09-19 12:54:54 +0300

    project 3 (discuss) has been generated and set successfully

--- course_one/project3-discuss/app/page.tsx
+++ course_one/project3-discuss/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@nextui-org/react";
 export default function Home() {
   return (
     <div>
-      <Button>Click Me!!</Button>{" "}
+      <Button>Click Me!!</Button>
     </div>
   );
 }
