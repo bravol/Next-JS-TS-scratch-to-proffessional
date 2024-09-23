2c69be3d179398bd7861feebb12b0f5774c91199
Brian Bravol <lumalabravo@gmail.com>
2024-09-23 13:48:07 +0300

    some files have been removed

--- course_one/project3-discuss/actions/sign-in.tsx
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return await auth.signIn("github");
-}
--- course_one/project3-discuss/app/topics/[slug]/posts/[postId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ShowPostPage() {
-  return <div>Show post</div>;
-}
