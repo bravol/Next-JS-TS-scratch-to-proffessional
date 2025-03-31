a5a9291a2eae143004ef385156fa3a82fe097664
Brian Bravol <lumalabravo@gmail.com>
2025-03-31 18:26:15 +0300

    connnected to prisma, myql database

--- course_one/ticket-management-system/app/tickets/page.tsx
+++ course_one/ticket-management-system/app/tickets/page.tsx
@@ -1,7 +1,15 @@
+import { prisma } from "@/prisma/db";
 import React from "react";
-
-const Tickets = () => {
-  return <div>Tickets</div>;
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+  return (
+    <div>
+      Tickets{" "}
+      {tickets.map((ticket) => (
+        <div key={ticket.id}>{ticket.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Tickets;
